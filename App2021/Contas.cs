69a75020c181a4c709b4b3aa84e00fed13020600
alberto <albertobarbosa0003@gmail.com>
2022-04-26 10:26:56 -0300

    Adicionar arquivos de projeto.

---
+++ App2021/Contas.cs
@@ -0,0 +1,11 @@
+﻿using App2021;
+
+ContasNumericas contasnumericas = new ContasNumericas();
+
+contasnumericas.Numero1 = int.Parse(Console.ReadLine());
+contasnumericas.Numero2 = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"Soma dos Numeros Digitados : {contasnumericas.Somar()}");
+Console.WriteLine($"Subtração dos Numeros Digitados : {contasnumericas.Subtrair()}");
+Console.WriteLine($"Mutiplicação dos Numeros Digitados : {contasnumericas.Mutiplicar()}");
+Console.WriteLine($"Divição dos Numeros Digitados : {contasnumericas.Dividir()}");
---
+++ App2021/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace App2021
+{
+    internal class ContasNumericas
+    {
+        public int Numero1 { get; set; }
+        public int Numero2 { get; set; }
+
+        public int Somar()
+        {
+            return Numero1 + Numero2;
+        }
+        public int Subtrair()
+        {
+            return Numero1 - Numero2;
+        }
+        public double Mutiplicar()
+        {
+            return Numero1 * Numero2;
+        }
+        public double Dividir()
+        {
+            return Numero1 / Numero2;
+        }
+    }
+}
